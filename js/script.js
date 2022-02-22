e702a511e609c8b88e9cd7ef933a345ff47ebc4d
juanframart2011 <juanframart2011@gmail.com>
2022-02-22 00:14:38 -0600

    finish course

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let button = document.getElementById( "icono" );
+let links = document.getElementById( "enlaces" );
+let contador = 0;
+button.addEventListener( "click", function(){
+
+	if( contador == 0 ){
+
+		enlaces.className = ( "enlaces dos" );
+		contador = 1;
+	}
+	else{
+		enlaces.classList.remove("dos");
+		enlaces.className = ( "enlaces uno" );
+		contador = 0;
+	}
+});
